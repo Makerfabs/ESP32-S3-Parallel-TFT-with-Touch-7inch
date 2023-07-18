12584fcfc984125f26164e533b63d47a333afc08
一九五 <2259761058@qq.com>
2023-07-18 15:46:58 +0800

    Update esp32s3_7_tft_fw_test.ino

--- example/esp32s3_7_tft_fw_test/esp32s3_7_tft_fw_test.ino
+++ example/esp32s3_7_tft_fw_test/esp32s3_7_tft_fw_test.ino
@@ -5,8 +5,8 @@
 #include <Audio.h>
 #include "JpegFunc.h"
 
-#define SCREEN_HD
-// #define SCREEN_NORMAL
+// #define SCREEN_HD
+#define SCREEN_NORMAL
 
 #define JPEG_FILENAME_LOGO "/logo.jpg"
 #define JPEG_FILENAME_COVER "/cover.jpg"
@@ -48,10 +48,20 @@ Arduino_ESP32RGBPanel *bus = new Arduino_ESP32RGBPanel(
     8 /* B0 */, 3 /* B1 */, 46 /* B2 */, 9 /* B3 */, 1 /* B4 */
 );
 
+#ifdef SCREEN_NORMAL
+Arduino_RPi_DPI_RGBPanel *gfx = new Arduino_RPi_DPI_RGBPanel(
+    bus,
+    800 /* width */, 0 /* hsync_polarity */, 210 /* hsync_front_porch */, 30 /* hsync_pulse_width */, 16 /* hsync_back_porch */,
+    480 /* height */, 0 /* vsync_polarity */, 22 /* vsync_front_porch */, 13 /* vsync_pulse_width */, 10 /* vsync_back_porch */,
+    1 /* pclk_active_neg */, 16000000 /* prefer_speed */, true /* auto_flush */);
+#endif
+
+#ifdef SCREEN_HD
 Arduino_RPi_DPI_RGBPanel *gfx = new Arduino_RPi_DPI_RGBPanel(
     bus,
     SCREEN_W /* width */, 1 /* hsync_polarity */, 40 /* hsync_front_porch */, 48 /* hsync_pulse_width */, 128 /* hsync_back_porch */,
     SCREEN_H /* height */, 1 /* vsync_polarity */, 13 /* vsync_front_porch */, 3 /* vsync_pulse_width */, 45 /* vsync_back_porch */);
+#endif
 
 TAMC_GT911 ts = TAMC_GT911(I2C_SDA_PIN, I2C_SCL_PIN, TOUCH_INT, TOUCH_RST, SCREEN_W, SCREEN_H);
 
